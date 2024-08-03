f09cc3a2274b8bb73663552a0cf052f6eae97783
dan-hoang <56205882+dan-hoang@users.noreply.github.com>
2024-08-02 17:50:40 -0700

    Update sprite-copier.js

--- scripts/nes/sprite-copier.js
+++ scripts/nes/sprite-copier.js
@@ -10,8 +10,6 @@ class SpriteCopier {
   writeToCpuAddress(address, data) {
     const page = data;
 
-    this._nes._vpu.writeToCpuAddress(0x2003, 0);
-
     for (let i = 0; i < 256; i++) {
       const spriteData = this._nes._cpuBus.readCpuAddress(page << 8 | i);
       this._nes._vpu.writeToCpuAddress(0x2004, spriteData);
